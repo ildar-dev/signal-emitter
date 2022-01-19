4870da99f06c1b122738fcc1dfbf0c88f4c86d76
IldarDavletyarov <ildardaf@gmail.com>
2022-01-20 00:26:21 +0300

    Update [env, close handler]

--- brokers/mt5/index.ts
+++ brokers/mt5/index.ts
@@ -4,13 +4,19 @@ import { db } from '../../mongodb';
 import { Logger, ELogLevel } from '../../logger';
 import config from '../../config.json';
 import MetaApi, { MetatraderAccount, MetatraderTradeResponse, PendingTradeOptions, StreamingMetaApiConnection } from 'metaapi.cloud-sdk';
+import { exit } from 'process';
 
 const logger = new Logger(ELogLevel.ALL, config.log.hasConsoleOutput, config.log.frequency, config.log.isEnable);
 
-const TOTAL_CASH = 1000;
+const TOTAL_CASH = process.env.TOTAL_CASH;
 
-const token = process.env.TOKEN || 'eyJhbGciOiJSUzUxMiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI0Zjg4OWMyYjI1MDUzNmVjNTQ0MTBlMDEzNzMwNmViNCIsInBlcm1pc3Npb25zIjpbXSwidG9rZW5JZCI6IjIwMjEwMjEzIiwiaWF0IjoxNjQxNjQ0OTY4LCJyZWFsVXNlcklkIjoiNGY4ODljMmIyNTA1MzZlYzU0NDEwZTAxMzczMDZlYjQifQ.dMQUii-iIwvg7h7hywN373EtJQt3fPf731Ixe9N7U0gF3WOuG5mvKqRYENi9_C8gWznd41aXv14d3ZyLjtIDrGKkHryMREeG66514Zd4VmZJQSTyHWof5glZ39-MyxsIzhIUxAosXhVwmoyt6x2sVOtWs1p0l3-8LO8dnU3Lhp58xFh-gUnLIHwRk1Vvs5JA3Fo8VbS6F285aQRQLTfKSLmYYVUCTgLAQsk6k-rtmYLId3i3Iwxr2zrGJxSBLjcRYQwiWgbHahfh8Jrw1pWbbQInbF0QxysTh8lWKmmWZZTClytdYwtOVZfRuikm7jUezMIVQJFbTVcVd-EQgLCYZQXkdeSoy90rxdyGjiM9jl9m78J2NvLdU6Vg8ibP7YvpPmZQALACsezFKuDdNJMn1CE8VJafzd1Vc64b4R3z7L3kqin3SL8Q2KkD8HwsUHoE-sxDyduU6kzHXYocO14h1eR4DtqrHlBuOGMPHoKA3Bkr7nQygqxFOsC_19n31Hdc3yfqK-aGdSxTo8DibgNv4cQfO9XBcDsW5Zpuw_lvIlsNeoAEMPFvwDDW-psmi7bYeibQFiI3vFzYKuVzmW1-e3EHTGqJ_ee9Tu3djzvM8pgS4htzJuHbyUGEch563Wu1RenhkovM6_ANewHsEpQzqsZ-4ANV2tQdH0qZX9ThVTU';
-const login = process.env.LOGIN || '67033143';
+const token = process.env.MT5_TOKEN as string;
+const login = process.env.MT5_LOGIN as string;
+
+if (!token || !login) {
+  console.error('ENV DOES NOT CONTAIN MT5_TOKEN, MT5_LOGIN');
+  exit(0);
+}
 
 const api = new MetaApi(token, {
   retryOpts: {
@@ -48,7 +54,6 @@ const handler: THandler = async (message: TMessage) => {
   const collection = db.collection(message.channelId + '_MT5');
   const ticker = message.ticker.split('.').join('');
   const TOTAL_QUANTITY = 0.01;
-  // Math.ceil(TOTAL_CASH * (message.percentage / 100));
   switch (message.type) {
     case EType.OPEN: {
       const options: PendingTradeOptions = {
@@ -91,9 +96,7 @@ const handler: THandler = async (message: TMessage) => {
       }
       let order: MetatraderTradeResponse | null = null;
       try {
-        order = await connection.closePosition(document.order.positionId, {
-          comment: `CLOSE price: ${message.price}; TP: ${message.takeProfit}; SL: ${message.stopLoss}`
-        }); // modificate
+        order = await connection.closePosition(document.order.positionId, {}); // modificate
       } catch(error) {
         console.error(error);
         logger.add(logOrderId, 'CLOSE PENDING ORDER', { document });
